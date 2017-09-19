d95ede73be8e9a52974bec7a2f25d45540dc6248
Sidharth <sidharth01g@gmail.com>
2017-09-19 22:29:19 +0530

    Cleaned up output

--- Misc/greedy_car_refuel.py
+++ Misc/greedy_car_refuel.py
@@ -1,5 +1,4 @@
 def get_min_refuels(x, capacity):
-    print('len: ', len(x))
     count = 0
     last_refuel = 0
     current = 0
@@ -7,15 +6,17 @@ def get_min_refuels(x, capacity):
     while True:
         while (current < len(x) - 1
                and (x[current + 1] - x[last_refuel] <= capacity)):
-            print(current)
             current += 1
 
         if current == last_refuel:
+            print('Cannot reach destination with current capacity ', capacity)
             return None
 
         if current < len(x) - 1:
             last_refuel = current
-            print('  ', last_refuel)
+            print(
+                'Refuel at station: ', last_refuel,
+                ' at distance ', x[last_refuel])
             count += 1
 
         if current == len(x) - 1:
@@ -24,7 +25,9 @@ def get_min_refuels(x, capacity):
 
 def main():
     x = [0, 21, 59, 68, 82, 93, 109, 125]
+    x = [0, 30, 41]
     c = 40
+    print('Fuel station distances: ', x)
     print('Min refuls: ', get_min_refuels(x, c))
 
 
