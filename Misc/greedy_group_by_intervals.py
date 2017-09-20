2f2166a21b621ae8d737d0f0f734c12a2bdc8bc4
Sidharth <sidharth01g@gmail.com>
2017-09-21 00:47:41 +0530

    Added interval grouping

---
+++ Misc/greedy_group_by_intervals.py
@@ -0,0 +1,33 @@
+# Uses numpy
+import numpy as np
+
+
+def get_groups(ages, interval):
+    ages = list(ages)
+    ages.sort()
+    print(ages)
+    interval = float(interval)
+    current = 0
+    groups = []
+    while current <= len(ages) - 1:
+        low = ages[current]
+        high = ages[current] + interval
+        groups.append((low, high))
+
+        while (current <= len(ages) - 1) and (ages[current] <= high):
+            current += 1
+    return groups
+
+
+def main():
+    interval = 5
+    ages = np.random.uniform(1, 100, size=(200))
+    print('Ages:\n', ages)
+    print('\nInterval: ', interval)
+    groups = get_groups(ages, interval)
+    print('\nNumber of groups: ', len(groups))
+    print('\nGroups:\n', groups)
+
+
+if __name__ == '__main__':
+    main()
